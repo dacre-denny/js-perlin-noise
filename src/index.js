aca2d8aaaebf5186dfc1f78b12009a198f6913d5
Dacre <enquiries@mooce.co.nz>
2018-12-16 17:11:51 +1300

    calculating gradients at boundaries

--- src/index.js
+++ src/index.js
@@ -5,6 +5,21 @@ canvas.setAttribute("height", 500);
 
 const context = canvas.getContext("2d");
 
+const gradientMap = {};
+
+const gradient = (x, y) => {
+    const key = `${x}${y}`;
+    if (!gradientMap[key]) {
+
+        const g = [Math.random(), Math.random()].map(d => d * 2 - 1);
+
+        const s = 1 / Math.sqrt(Math.pow(g[0], 2) + Math.pow(g[1], 2));
+
+        gradientMap[key] = g.map(d => d * s);
+    }
+    return gradientMap[key];
+};
+
 const noise2d = (x, y) => {
 
     const ix = parseInt(x);
@@ -13,17 +28,27 @@ const noise2d = (x, y) => {
     const fx = x - ix;
     const fy = y - iy;
 
+
     if (fx === 0 && fy === 0) {
-        return "red";
+
+        const g = gradient(ix, iy);
+        //console.log("x" + x, "y" + y);
+        context.beginPath();
+        context.moveTo(x * 100, y * 100);
+        context.lineTo(x * 100 + g[0] * 10, y * 100 + g[1] * 10);
+        context.stroke();
     }
     if (fx === 0) {
-        return "white";
+        // context.fillStyle = "white";
+        // context.fillRect(i * 100, j * 100, 1, 1);
     }
     if (fy === 0) {
-        return "white";
+        // context.fillStyle = "white";
+        // context.fillRect(i * 100, j * 100, 1, 1);
     }
 
-    return "blue";
+    // context.fillStyle = "blue";
+    // context.fillRect(i * 100, j * 100, 1, 1);
 };
 
 const onDraw = () => {
@@ -31,15 +56,15 @@ const onDraw = () => {
     for (var i = 0; i < 500; i++) {
         for (var j = 0; j < 500; j++) {
 
-            const x = 5 * i / 500;
-            const y = 5 * j / 500;
+            const x = i / 100;
+            const y = j / 100;
 
             context.fillStyle = noise2d(x, y);
-            context.fillRect(i, j, 1, 1);
+            //   context.fillRect(i, j, 1, 1);
         }
     }
 
-    requestAnimationFrame(onDraw);
+    //   requestAnimationFrame(onDraw);
 };
 
 onDraw();
