8d63b54d223e231f655a3a3c40a857d5d5a871f7
Dacre <enquiries@mooce.co.nz>
2018-12-16 17:54:58 +1300

    added vector methods to support noise calculation

--- src/index.js
+++ src/index.js
@@ -7,54 +7,74 @@ const context = canvas.getContext("2d");
 
 const gradientMap = {};
 
-const gradient = (x, y) => {
+const gradient = ([x, y]) => {
     const key = `${x}${y}`;
     if (!gradientMap[key]) {
 
         const g = [Math.random(), Math.random()].map(d => d * 2 - 1);
 
-        const s = 1 / Math.sqrt(Math.pow(g[0], 2) + Math.pow(g[1], 2));
-
-        gradientMap[key] = g.map(d => d * s);
+        gradientMap[key] = g;
     }
+
     return gradientMap[key];
+};
+
+const normalize = (v) => {
+
+    const len = Math.sqrt(v[0] * v[0] + v[1] * v[1]);
+
+    return [v[0] / len, v[1] / len];
+};
+
+const dot = (v0, v1) => {
+
+    return v0[0] * v1[0] + v0[1] * v1[1];
+};
+
+const sub = (v0, v1) => {
+
+    return [v0[0] - v1[0], v0[1] - v1[1]];
 };
 
 const noise2d = (x, y) => {
 
     const ix = parseInt(x);
     const iy = parseInt(y);
+    const sample = [x, y];
 
-    const fx = x - ix;
-    const fy = y - iy;
+    const cTL = [ix + 0, iy + 0];
+    const cTR = [ix + 1, iy + 0];
+    const cBL = [ix + 0, iy + 1];
+    const cBR = [ix + 1, iy + 1];
 
+    const gTL = normalize(gradient(cTL));
+    const gTR = normalize(gradient(cTR));
+    const gBL = normalize(gradient(cBL));
+    const gBR = normalize(gradient(cBR));
 
-    if (fx === 0 && fy === 0) {
+    const vTL = normalize(sub(sample, cTL));
+    const vTR = normalize(sub(sample, cTR));
+    const vBL = normalize(sub(sample, cBL));
+    const vBR = normalize(sub(sample, cBR));
 
-        const g = gradient(ix, iy);
-        //console.log("x" + x, "y" + y);
-        context.beginPath();
-        context.moveTo(x * 100, y * 100);
-        context.lineTo(x * 100 + g[0] * 10, y * 100 + g[1] * 10);
-        context.stroke();
-    }
-    if (fx === 0) {
-        // context.fillStyle = "white";
-        // context.fillRect(i * 100, j * 100, 1, 1);
-    }
-    if (fy === 0) {
-        // context.fillStyle = "white";
-        // context.fillRect(i * 100, j * 100, 1, 1);
-    }
+    const dTL = dot(gTL, vTL);
+    const dTR = dot(gTR, vTR);
+    const dBL = dot(gBL, vBL);
+    const dBR = dot(gBR, vBR);
 
-    // context.fillStyle = "blue";
-    // context.fillRect(i * 100, j * 100, 1, 1);
+    const avgT = (dTL + dTR) * 0.5;
+    const avgB = (dBL + dBR) * 0.5;
+    const avg = (avgT + avgB) * 0.5;
+    const value = avg * 255;
+
+    context.fillStyle = `rgb(${value},${value},${value})`;
+    context.fillRect(x * 100, y * 100, 1, 1);
 };
 
 const onDraw = () => {
 
-    for (var i = 0; i < 500; i++) {
-        for (var j = 0; j < 500; j++) {
+    for (var i = 0; i < 300; i++) {
+        for (var j = 0; j < 300; j++) {
 
             const x = i / 100;
             const y = j / 100;
